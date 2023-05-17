8470aee776549d025acb753c0be742d0f4edfa05
tylermorganwall <tylermorganwall@users.noreply.github.com>
2023-05-17 00:49:55 -0400

    -Update docs

--- R/get_trunk_file.R
+++ R/get_trunk_file.R
@@ -2,7 +2,8 @@
 #'
 #' @description This function retrieves a trunk OBJ file. The trunk OBJ file can be used to create a tree trunk in a 3D scene. The function has an option to scale the trunk model based on the radius.
 #'
-#' @param scaled Default `FALSE`. This parameter determines whether the trunk model should be scaled from a radius of 0.1 to 1. If TRUE, the trunk model will be scaled; otherwise, it will use the original size.
+#' @param scaled Default `TRUE`. This parameter determines whether the trunk model should be scaled from a radius of 0.1 to 1. If TRUE, the trunk model will be scaled; otherwise, it will use the original size.
+#' @param offset Default `TRUE`. This parameter determines whether the trunk model should be offset to start at the origin, or centered at the origin (`FALSE`).
 #' @export
 #'
 #' @return Returns the filename of the selected OBJ file (as a .txt file) for the tree trunk. This file can be used to create tree trunk models in a 3D scene.
