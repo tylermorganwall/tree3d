41f877f7eed7f9026e4100313af04e1d31d040d0
tylermorganwall <tylermorganwall@users.noreply.github.com>
2023-05-27 09:37:51 -0400

    -`tree_mesh()` Add tree_height and trunk_height_ratio arguments and use that as the default method to set the overall tree proportions

--- R/tree_mesh.R
+++ R/tree_mesh.R
@@ -27,15 +27,17 @@
 #'`"medium"`
 #'`"high`
 #' @param filename Default `NULL`. File name of the OBJ file, if saving the mesh to a local file.
-#' @param crown_height   Default `1`. A numeric value setting the height of the crown.
-#' @param crown_diameter Default `1`. A numeric value setting the diameter of the crown.
-#' @param trunk_height   Default `1`. A numeric value setting the height of the trunk.
-#' @param trunk_diameter Default `0.1`. A numeric value setting the diameter of the trunk.
-#' @param crown_color    Default `NA`, use default for crown type. A string specifying the hex code of the crown color.
-#' @param trunk_color    Default `"#8C6F5B"`. A string specifying the hex code of the trunk color.
+#' @param tree_height Default `1`. A numeric value setting the total height of the tree.
+#' @param trunk_height_ratio Default `1/3`. A numeric value specifying the ratio of the trunk height to the total height of the tree.
+#' @param crown_width_ratio Default `1`. A numeric value specifying the ratio of the crown width to the crown height.
+#' @param crown_height Default `NULL`. A numeric value setting the height of the crown. If not provided, it is calculated based on the tree height and trunk height ratio.
+#' @param crown_width Default `NULL`. A numeric value setting the diameter of the crown. If not provided, it is calculated based on the crown height and crown width ratio.
+#' @param trunk_height Default `NULL`. A numeric value setting the height of the trunk. If not provided, it is calculated based on the tree height and trunk height ratio.
+#' @param trunk_width Default `NULL`. A numeric value setting the diameter of the trunk. If not provided, this is set to 1/5th the trunk height.
+#' @param crown_color Default `NA`, use default for crown type. A string specifying the hex code of the crown color.
+#' @param trunk_color Default `"#8C6F5B"`. A string specifying the hex code of the trunk color.
 #' @param diffuse_intensity Default `1.0`. A numeric value controlling the amount of diffuse (shaded) color included in the model.
 #' @param ambient_intensity Default `0.2`. A numeric value controlling the amount of ambient (constant) color included in the model.
-#'
 #'@export
 #'
 #'@return `ray_mesh` list object
@@ -53,8 +55,9 @@
 #'                    light_info = directional_light(c(0.3,1,1), intensity = 0.7),
 #'                    lookfrom=c(0,3,10),
 #'                    fov=10,
-#'                    shadow_map_dims = 2,
-#'                    shadow_map_bias = 0.0005,
+#'                    shadow_map_dims = 2, shadow_map_bias = 0.0005,
+#'                    width = 800, height = 800,
+#'                    fsaa = 4, ssao = TRUE, ssao_intensity = 1,
 #'                    background = "lightblue")
 #'}
 #'if(tree3d:::run_documentation()) {
@@ -72,12 +75,37 @@
 #'  render_tree_example()
 #'}
 #'if(tree3d:::run_documentation()) {
-#'#Change the trunk and crown dimensions
+#'#Adjust the trunk/crown proportions of the overall tree using ratios
+#'tree_mesh("columnar",
+#'          tree_height = 1,
+#'          trunk_height_ratio = 1/5,
+#'          crown_width_ratio = 1.25,
+#'          trunk_width = 0.125,
+#'          solid = TRUE,
+#'          ambient_intensity = 0.3) |>
+#'  render_tree_example()
+#'
+#'}
+#'
+#'if(tree3d:::run_documentation()) {
+#'#The crown width is set proportional to the crown height (not the overall tree)
+#'tree_mesh("columnar",
+#'          tree_height = 1,
+#'          trunk_height_ratio = 2/3,
+#'          crown_width_ratio = 1,
+#'          trunk_width = 0.075,
+#'          solid = TRUE,
+#'          ambient_intensity = 0.3) |>
+#'  render_tree_example()
+#'}
+#'
+#'if(tree3d:::run_documentation()) {
+#'#Set the trunk and crown dimensions directly
 #'tree_mesh("columnar",
 #'          trunk_height = 0.25,
-#'          trunk_diameter = 0.05,
-#'          crown_height = 1,
-#'          crown_diameter = 0.5,
+#'          trunk_width = 0.05,
+#'          crown_height = 0.75,
+#'          crown_width = 0.33,
 #'          solid = TRUE,
 #'          ambient_intensity = 0.3) |>
 #'  render_tree_example()
@@ -194,10 +222,13 @@ tree_mesh = function(crown_type = "oval",
                      solid = FALSE,
                      resolution = "medium",
                      filename = NULL,
-                     crown_height = 1,
-                     crown_diameter  = 1,
-                     trunk_height = 0.5,
-                     trunk_diameter = 0.1,
+                     tree_height  = 1,
+                     trunk_height_ratio = 1/3,
+                     crown_width_ratio = 1,
+                     crown_height = NULL,
+                     crown_width = NULL,
+                     trunk_height = NULL,
+                     trunk_width = NULL,
                      crown_color = NA,
                      trunk_color = "#8C6F5B",
                      diffuse_intensity = 1.0,
@@ -212,13 +243,30 @@ tree_mesh = function(crown_type = "oval",
     crown_color = get_default_tree_color(crown_type)
   }
 
+  #Define crown dimensions
+  if(is.null(crown_height)) {
+    stopifnot(trunk_height_ratio < 1 && trunk_height_ratio >= 0)
+    crown_height = (1 - trunk_height_ratio) * tree_height
+  }
+  if(is.null(crown_width)) {
+    crown_width = crown_height * crown_width_ratio
+  }
+
+  #Define trunk dimensions
+  if(is.null(trunk_height)) {
+    trunk_height = trunk_height_ratio * tree_height
+  }
+  if(is.null(trunk_width)) {
+    trunk_width = trunk_height / 5
+  }
+
   crown_mesh = rayvertex::obj_mesh(crown_file,
                                    material = rayvertex::material_list(diffuse = crown_color,
                                                                        ambient = crown_color,
                                                                        diffuse_intensity = diffuse_intensity,
                                                                        ambient_intensity = ambient_intensity,
                                                                        culling = "none", two_sided = TRUE)) |>
-    rayvertex::scale_mesh(scale = c(crown_diameter,crown_height,crown_diameter)) |>
+    rayvertex::scale_mesh(scale = c(crown_width,crown_height,crown_width)) |>
     rayvertex::translate_mesh(position = c(0,trunk_height,0))
 
   trunk_mesh = rayvertex::obj_mesh(get_trunk_file(offset = TRUE),
@@ -226,9 +274,9 @@ tree_mesh = function(crown_type = "oval",
                                                                        diffuse_intensity = diffuse_intensity,
                                                                        ambient_intensity = ambient_intensity,
                                                                        ambient = trunk_color)) |>
-    rayvertex::scale_mesh(scale = c(trunk_diameter,
+    rayvertex::scale_mesh(scale = c(trunk_width,
                                     trunk_height,
-                                    trunk_diameter))
+                                    trunk_width))
   tree_full = rayvertex::add_shape(crown_mesh, trunk_mesh) |>
     rayvertex::rotate_mesh(angle = c(0,angle,0)) |>
     rayvertex::translate_mesh(position)
